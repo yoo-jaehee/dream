186e2b259b72b60ef22df543b498ab7ad2813317
yoo-jaehee <uujhee@gmail.com>
2025-02-12 19:40:12 +0900

    앱카드 컴포넌트로 만들어서 공통으로 사용

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppWrap from "./AppWrap";
+import AppCard from "./AppCard";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppWrap />
+    <AppCard />
   </React.StrictMode>
 );
 
